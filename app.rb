226dc870031127d602e8a439a599b5654b514d87
Saint-Jean Germila <germila@noukod.com>
2019-01-18 17:34:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+end
+
+  post '/checkout' do
+    session["item"] = @params[:"item"]
+    @session = session
+
+    Item.new(item[:name])
+    @item=Item.all
+
+end
+
+get '/' do
+
+erb :index
+end
 end
