1bd640e1c4f7cbb5051b238af2378a75cde29df9
Saint-Jean Germila <germila@noukod.com>
2019-01-18 17:42:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, "flatiron"
 end
 
 get '/' do
@@ -12,7 +12,7 @@ get '/' do
 erb :index
 end
 
-post '/checkout' do 
+post '/checkout' do
     session[:item] = params[:item]
     @session = session
     erb :checkout
